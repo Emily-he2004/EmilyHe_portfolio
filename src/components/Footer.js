b9551ef079cafed17c563e49d196f5a3dc2e90d1
Emily He <emmyemmyhe@gmail.com>
2024-03-17 16:44:27 -0700

    fixed side navs to stay in view near botton when user scrolls

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,9 @@
 import React from "react";
-import LeftNav from "../components/LeftNav";
-import RightNav from "./RightNav";
 
 function Footer() {
   return (
     <div className="bottom-nav-container">
-      {/* <LeftNav /> */}
       <div className="footer-container">&#169; Emily He 2024</div>
-      {/* <RightNav /> */}
     </div>
   );
 }
