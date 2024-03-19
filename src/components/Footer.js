bc1ef49934f45e78229e112ad7777d3b61dfcafa
Emily He <emmyemmyhe@gmail.com>
2024-03-19 10:16:44 -0700

    header and footer styling and script update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <div className="bottom-nav-container">
-      <div className="footer-container">&#169; Emily He 2024</div>
-    </div>
-  );
+  return <div className="footer-container">&#169; Emily He 2024</div>;
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,32 @@
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import LeftNav from "../components/LeftNav";
-import RightNav from "./RightNav";
+import RightNav from "../components/RightNav";
 
 function Header() {
+  const [prevScrollPos, setPrevScrollPos] = useState(window.scrollY || window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY || window.pageYOffset;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible]);
+
   return (
     <div className="header-container">
       <LeftNav />
 
-      <nav className="header-nav">
+      <nav className={`header-nav ${visible ? "visible" : "hidden"}`}>
         <ul className="home-menu">
           <li>
             <NavLink to="/">
-              <br></br>
+              <br />
               Emily.Dev
             </NavLink>
           </li>
@@ -19,27 +34,28 @@ function Header() {
         <ul className="nav-menu">
           <li>
             <NavLink to="/about">
-              <br></br>
+              <br />
               About
             </NavLink>
           </li>
           <li>
             <NavLink to="/projects">
-              <br></br>
+              <br />
               Projects
             </NavLink>
           </li>
           <li>
             <NavLink to="/contact">
-              <br></br>
+              <br />
               Contact
             </NavLink>
           </li>
         </ul>
       </nav>
-      
+
       <RightNav />
     </div>
   );
 }
+
 export default Header;
